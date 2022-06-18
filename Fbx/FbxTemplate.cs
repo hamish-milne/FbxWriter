50c9d5a77b41f80a7f7e66d644fa9844c8643780
RoyTheunissen <roy.theunissen@live.nl>
2022-06-18 22:32:13 +0200

    Added a template for creating a basic valid FBX file.

---
+++ Fbx/FbxTemplate.cs
@@ -0,0 +1,340 @@
+using System;
+using System.IO;
+using Fbx.PropertyBlocks;
+
+namespace Fbx
+{
+	/// <summary>
+	/// Template for writing valid FBX files. The intention is to set it up to recreate a very basic reference file
+	/// and then gradually make parts of that file dynamic and support real data.
+	/// </summary>
+	public class FbxTemplate
+	{
+		private const string ApplicationName = "FbxWriter-ForAnimations";
+		private const string ApplicationVersion = "000001";
+		private const string VendorName = "YourNameHere";
+
+		private readonly string path;
+		private readonly FbxDocument fbxDocument;
+
+		/// <summary>
+		/// Create a new FBX Template.
+		/// </summary>
+		/// <param name="path">The file path ending with .fbx that the data will be written to.</param>
+		public FbxTemplate(string path)
+		{
+			this.path = path;
+
+			// Create a document.
+			fbxDocument = new FbxDocument { Version = FbxVersion.v7_5 };
+		}
+
+		/// <summary>
+		/// Writes the data to an FBX file.
+		/// </summary>
+		public void Write()
+		{
+			// Open an FBX writer.
+			FileStream fileStream = new FileStream(path, FileMode.Create);
+			FbxAsciiWriter writer = new FbxAsciiWriter(fileStream);
+
+			CreateHeader();
+			CreateGlobalSettings();
+			CreateDocumentsDescription();
+			CreateDocumentReferences();
+			CreateObjectDefinitions();
+			CreateObjectProperties();
+			CreateObjectConnections();
+			CreateTakes();
+
+			// Close the FBX writer.
+			writer.Write(fbxDocument);
+			fileStream.Close();
+		}
+
+		private void CreateHeader()
+		{
+			// Add a header node.
+			FbxNode headerNode = fbxDocument.Add("FBXHeaderExtension");
+
+			headerNode.Add("FBXHeaderVersion", 1003);
+			headerNode.Add("FBXVersion", 7500);
+
+			FbxNode creationTimeStamp = headerNode.Add("CreationTimeStamp");
+			creationTimeStamp.Add("Version", 1000);
+			DateTime currentTime = DateTime.Now;
+			creationTimeStamp.Add("Year", currentTime.Year);
+			creationTimeStamp.Add("Month", currentTime.Month);
+			creationTimeStamp.Add("Day", currentTime.Day);
+			creationTimeStamp.Add("Hour", currentTime.Hour);
+			creationTimeStamp.Add("Minute", currentTime.Minute);
+			creationTimeStamp.Add("Second", currentTime.Second);
+			creationTimeStamp.Add("Millisecond", currentTime.Millisecond);
+
+			headerNode.Add("Creator", "FbxWriter-ForAnimations");
+
+			FbxNode sceneInfo = headerNode.Add("SceneInfo", "SceneInfo::GlobalInfo", "UserData");
+			sceneInfo.Add("Type", "UserData");
+			sceneInfo.Add("Version", 100);
+
+			FbxNode metaData = sceneInfo.Add("MetaData");
+			metaData.Add("Version", 100);
+			metaData.Add("Title", "");
+			metaData.Add("Subject", "");
+			metaData.Add("Author", "");
+			metaData.Add("Keywords", "");
+			metaData.Add("Revision", "");
+			metaData.Add("Comment", "");
+
+			PropertyBlock properties = new PropertyBlock(sceneInfo);
+			properties.AddString("DocumentUrl", $@"{path}", StringTypes.Url);
+			properties.AddString("SrcDocumentUrl", $@"{path}", StringTypes.Url);
+
+			CompoundProperty originalCompound = properties.AddCompound("Original");
+			originalCompound.AddString("ApplicationVendor", VendorName);
+			originalCompound.AddString("ApplicationName", ApplicationName);
+			originalCompound.AddString("ApplicationVersion", ApplicationVersion);
+			originalCompound.AddDateTime("DateTime_GMT", currentTime);
+			originalCompound.AddString("FileName", path);
+
+			CompoundProperty lastSavedCompound = properties.AddCompound("LastSaved");
+			lastSavedCompound.AddString("ApplicationVendor", VendorName);
+			lastSavedCompound.AddString("ApplicationName", ApplicationName);
+			lastSavedCompound.AddString("ApplicationVersion", ApplicationVersion);
+			lastSavedCompound.AddDateTime("DateTime_GMT", currentTime);
+
+			originalCompound.AddString("ApplicationActiveProject", Path.GetDirectoryName(path));
+		}
+
+		private void CreateGlobalSettings()
+		{
+			FbxNode globalSettings = fbxDocument.Add("GlobalSettings");
+			globalSettings.Add("Version", 1000);
+
+			PropertyBlock properties = new PropertyBlock(globalSettings);
+			properties.AddInteger("UpAxis", 1);
+			properties.AddInteger("UpAxisSign", 1);
+			properties.AddInteger("FrontAxis", 2);
+			properties.AddInteger("FrontAxisSign", 1);
+			properties.AddInteger("CoordAxis", 0);
+			properties.AddInteger("CoordAxisSign", 1);
+			properties.AddInteger("OriginalUpAxis", 1);
+			properties.AddInteger("OriginalUpAxisSign", 1);
+			properties.AddDouble("UnitScaleFactor", 1);
+			properties.AddDouble("OriginalUnitScaleFactor", 1);
+			properties.AddColorRGB("AmbientColor", new ColorRGB(0, 0, 0));
+			properties.AddString("DefaultCamera", "Producer Perspective");
+			properties.AddEnum("TimeMode", 6);
+			properties.AddEnum("TimeProtocol", 2);
+			properties.AddEnum("SnapOnFrameMode", 0);
+			properties.AddTime("TimeSpanStart", new DateTime(1539538600));
+			properties.AddTime("TimeSpanStop", new DateTime(92372316000));
+			properties.AddDouble("CustomFrameRate", -1);
+			properties.AddCompound("TimeMarker");
+			properties.AddInteger("CurrentTimeMarker", -1);
+		}
+
+		private void CreateDocumentsDescription()
+		{
+			fbxDocument.AddComment("Documents Description", true);
+			FbxNode documents = fbxDocument.Add("Documents");
+			documents.Add("Count", 1);
+
+			FbxNode document = documents.Add("Document", 1710616802960, "", "Scene");
+
+			PropertyBlock properties = new PropertyBlock(document);
+			properties.AddObject("SourceObject", null);
+			properties.AddString("ActiveAnimStackName", "Take 001");
+
+			document.Add("RootNode", 0);
+		}
+
+		private void CreateDocumentReferences()
+		{
+			fbxDocument.AddComment("Document References", true);
+			FbxNode references = fbxDocument.Add("References");
+			references.Add(null);
+		}
+
+		private void CreateObjectDefinitions()
+		{
+			fbxDocument.AddComment("Object definitions", true);
+			FbxNode definitions = fbxDocument.Add("Definitions");
+			definitions.Add("Version", 100);
+			definitions.Add("Count", 5);
+
+			FbxNode globalSettings = definitions.Add("ObjectType", "GlobalSettings");
+			globalSettings.Add("Count", 1);
+
+			FbxNode animationStack = definitions.Add("ObjectType", "AnimationStack");
+			animationStack.Add("Count", 1);
+			FbxNode propertyTemplate = animationStack.Add("PropertyTemplate", "FbxAnimStack");
+			PropertyBlock propertyBlock = new PropertyBlock(propertyTemplate);
+			propertyBlock.AddString("Description", "");
+			propertyBlock.AddTime("LocalStart", DateTime.MinValue);
+			propertyBlock.AddTime("LocalStop", DateTime.MinValue);
+			propertyBlock.AddTime("ReferenceStart", DateTime.MinValue);
+			propertyBlock.AddTime("ReferenceStop", DateTime.MinValue);
+
+			FbxNode animationLayer = definitions.Add("ObjectType", "AnimationLayer");
+			animationLayer.Add("Count", 1);
+			propertyTemplate = animationLayer.Add("PropertyTemplate", "FbxAnimLayer");
+			propertyBlock = new PropertyBlock(propertyTemplate);
+			propertyBlock.AddNumber("Weight", 100);
+			propertyBlock.AddBool("Mute", false);
+			propertyBlock.AddBool("Solo", false);
+			propertyBlock.AddBool("Lock", false);
+			propertyBlock.AddColorRGB("Color", new ColorRGB(0.8f, 0.8f, 0.8f));
+			propertyBlock.AddEnum("BlendMode", 0);
+			propertyBlock.AddEnum("RotationAccumulationMode", 0);
+			propertyBlock.AddEnum("ScaleAccumulationMode", 0);
+			propertyBlock.AddULongLong("BlendModeBypass", 0);
+
+			FbxNode nodeAttribute = definitions.Add("ObjectType", "NodeAttribute");
+			nodeAttribute.Add("Count", 1);
+			propertyTemplate = nodeAttribute.Add("PropertyTemplate", "FbxSkeleton");
+			propertyBlock = new PropertyBlock(propertyTemplate);
+			propertyBlock.AddColorRGB("Color", new ColorRGB(0.8f, 0.8f, 0.8f));
+			propertyBlock.AddDouble("Size", 100);
+			propertyBlock.AddDouble("LimbLength", 100, DoubleTypes.H);
+
+			FbxNode model = definitions.Add("ObjectType", "Model");
+			model.Add("Count", 1);
+			propertyTemplate = model.Add("PropertyTemplate", "FbxNode");
+			propertyBlock = new PropertyBlock(propertyTemplate);
+			propertyBlock.AddEnum("QuaternionInterpolate", 0);
+			propertyBlock.AddVector3D("RotationOffset", Vector3D.Zero);
+			propertyBlock.AddVector3D("RotationPivot", Vector3D.Zero);
+			propertyBlock.AddVector3D("ScalingOffset", Vector3D.Zero);
+			propertyBlock.AddVector3D("ScalingPivot", Vector3D.Zero);
+			propertyBlock.AddBool("TranslationActive", false);
+			propertyBlock.AddVector3D("TranslationMin", Vector3D.Zero);
+			propertyBlock.AddVector3D("TranslationMax", Vector3D.Zero);
+			propertyBlock.AddBool("TranslationMinX", false);
+			propertyBlock.AddBool("TranslationMinY", false);
+			propertyBlock.AddBool("TranslationMinZ", false);
+			propertyBlock.AddBool("TranslationMaxX", false);
+			propertyBlock.AddBool("TranslationMaxY", false);
+			propertyBlock.AddBool("TranslationMaxZ", false);
+			propertyBlock.AddEnum("RotationOrder", 0);
+			propertyBlock.AddBool("RotationSpaceForLimitOnly", false);
+			propertyBlock.AddDouble("RotationStiffnessX", 0);
+			propertyBlock.AddDouble("RotationStiffnessY", 0);
+			propertyBlock.AddDouble("RotationStiffnessZ", 0);
+			propertyBlock.AddDouble("AxisLen", 10);
+			propertyBlock.AddVector3D("PreRotation", Vector3D.Zero);
+			propertyBlock.AddVector3D("PostRotation", Vector3D.Zero);
+			propertyBlock.AddBool("RotationActive", false);
+			propertyBlock.AddVector3D("RotationMin", Vector3D.Zero);
+			propertyBlock.AddVector3D("RotationMax", Vector3D.Zero);
+			propertyBlock.AddBool("RotationMinX", false);
+			propertyBlock.AddBool("RotationMinY", false);
+			propertyBlock.AddBool("RotationMinZ", false);
+			propertyBlock.AddBool("RotationMaxX", false);
+			propertyBlock.AddBool("RotationMaxY", false);
+			propertyBlock.AddBool("RotationMaxZ", false);
+			propertyBlock.AddEnum("InheritType", 0);
+			propertyBlock.AddBool("ScalingActive", false);
+			propertyBlock.AddVector3D("ScalingMin", Vector3D.Zero);
+			propertyBlock.AddVector3D("ScalingMax", Vector3D.One);
+			propertyBlock.AddBool("ScalingMinX", false);
+			propertyBlock.AddBool("ScalingMinY", false);
+			propertyBlock.AddBool("ScalingMinZ", false);
+			propertyBlock.AddBool("ScalingMaxX", false);
+			propertyBlock.AddBool("ScalingMaxY", false);
+			propertyBlock.AddBool("ScalingMaxZ", false);
+			propertyBlock.AddVector3D("GeometricTranslation", Vector3D.Zero);
+			propertyBlock.AddVector3D("GeometricRotation", Vector3D.Zero);
+			propertyBlock.AddVector3D("GeometricScaling", Vector3D.One);
+			propertyBlock.AddDouble("MinDampRangeX", 0);
+			propertyBlock.AddDouble("MinDampRangeY", 0);
+			propertyBlock.AddDouble("MinDampRangeZ", 0);
+			propertyBlock.AddDouble("MaxDampRangeX", 0);
+			propertyBlock.AddDouble("MaxDampRangeY", 0);
+			propertyBlock.AddDouble("MaxDampRangeZ", 0);
+			propertyBlock.AddDouble("MinDampStrengthX", 0);
+			propertyBlock.AddDouble("MinDampStrengthY", 0);
+			propertyBlock.AddDouble("MinDampStrengthZ", 0);
+			propertyBlock.AddDouble("MaxDampStrengthX", 0);
+			propertyBlock.AddDouble("MaxDampStrengthY", 0);
+			propertyBlock.AddDouble("MaxDampStrengthZ", 0);
+			propertyBlock.AddDouble("PreferedAngleX", 0); // There's a typo in here but that's part of the format...
+			propertyBlock.AddDouble("PreferedAngleY", 0); // There's a typo in here but that's part of the format...
+			propertyBlock.AddDouble("PreferedAngleZ", 0); // There's a typo in here but that's part of the format...
+			propertyBlock.AddObject("LookAtProperty", null);
+			propertyBlock.AddObject("UpVectorProperty", null);
+			propertyBlock.AddBool("Show", true);
+			propertyBlock.AddBool("NegativePercentShapeSupport", true);
+			propertyBlock.AddInteger("DefaultAttributeIndex", -1);
+			propertyBlock.AddBool("Freeze", false);
+			propertyBlock.AddBool("LODBox", false);
+			propertyBlock.AddLclTranslation("Lcl Translation", Vector3D.Zero);
+			propertyBlock.AddLclTranslation("Lcl Rotation", Vector3D.Zero);
+			propertyBlock.AddLclTranslation("Lcl Scaling", Vector3D.One);
+			propertyBlock.AddVisibility("Visibility", true);
+			propertyBlock.AddVisibilityInheritance("Visibility Inheritance", true);
+		}
+
+		private void CreateObjectProperties()
+		{
+			fbxDocument.AddComment("Object properties", true);
+			FbxNode objects = fbxDocument.Add("Objects");
+
+			FbxNode limbNode = objects.Add("NodeAttribute", 1710781923776, "NodeAttribute::", "LimbNode");
+			PropertyBlock propertyBlock = new PropertyBlock(limbNode);
+			propertyBlock.AddDouble("Size", 333.333333333333);
+			limbNode.Add("TypeFlags", "Skeleton");
+
+			FbxNode joint1 = objects.Add("Model", 1710676519120, "Model::joint1", "LimbNode");
+			joint1.Add("Version", 232);
+			propertyBlock = new PropertyBlock(joint1);
+			propertyBlock.AddBool("RotationActive", true);
+			propertyBlock.AddEnum("InheritType", 1);
+			propertyBlock.AddVector3D("ScalingMax", Vector3D.Zero);
+			propertyBlock.AddInteger("DefaultAttributeIndex", 0);
+			propertyBlock.AddLclTranslation("Lcl Translation", new Vector3D(0, 0, 100));
+			joint1.Add("Shading", 'Y');
+			joint1.Add("Culling", "CullingOff");
+
+			FbxNode animationStack = objects.Add("AnimationStack", 1710772262592, "AnimStack::Take 001", "");
+			propertyBlock = new PropertyBlock(animationStack);
+			propertyBlock.AddTime("LocalStart", new DateTime(1539538600));
+			propertyBlock.AddTime("LocalStop", new DateTime(46186158000));
+			propertyBlock.AddTime("ReferenceStart", new DateTime(1539538600));
+			propertyBlock.AddTime("ReferenceStop", new DateTime(46186158000));
+
+			FbxNode animationLayer = objects.Add("AnimationLayer", 1710601890624, "AnimLayer::BaseLayer", "");
+			animationLayer.Add(null);
+		}
+
+		private void CreateObjectConnections()
+		{
+			fbxDocument.AddComment("Object connections", true);
+			FbxNode connections = fbxDocument.Add("Connections");
+
+			// Weird format, man.
+			connections.AddLineBreak();
+			connections.AddComment("Model::joint1, Model::RootNode", false, false);
+			connections.Add("C", "OO", 1710676519120, 0);
+
+			connections.AddLineBreak();
+			connections.AddComment("AnimLayer::BaseLayer, AnimStack::Take 001", false, false);
+			connections.Add("C", "OO", 1710601890624, 1710772262592);
+
+			connections.AddLineBreak();
+			connections.AddComment("NodeAttribute::, Model::joint1", false, false);
+			connections.Add("C", "OO", 1710781923776, 1710676519120);
+		}
+
+		private void CreateTakes()
+		{
+			fbxDocument.AddComment("Takes section", true);
+			FbxNode takes = fbxDocument.Add("Takes");
+			takes.Add("Current", "Take 001");
+			FbxNode take001 = takes.Add("Take", "Take 001");
+			take001.Add("FileName", "Take_001.tak");
+			take001.Add("LocalTime", 1539538600, 46186158000);
+			take001.Add("ReferenceTime", 1539538600, 46186158000);
+		}
+	}
+}
